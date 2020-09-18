106552e378fe1242a5a861cc5345fd54a8d76f4c
Oguzhan Atalay <oguzhnatly@gmail.com>
2020-09-18 12:53:47 +0300

    Finished v2.4

---
+++ json-server/public/precache-manifest.8eb029cdf8654855fbe1d5bea7b1951d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01fe48ac416bd813e891d9366cb04e67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43fa3239d44f32c49548",
+    "url": "/static/css/2.58f81ebc.chunk.css"
+  },
+  {
+    "revision": "433c9eb48e29bc31a82b",
+    "url": "/static/css/main.c51fa473.chunk.css"
+  },
+  {
+    "revision": "43fa3239d44f32c49548",
+    "url": "/static/js/2.ec1c07d3.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.ec1c07d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433c9eb48e29bc31a82b",
+    "url": "/static/js/main.19735495.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
